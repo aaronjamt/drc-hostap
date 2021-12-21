b6561e23a1ab3b9d40647584fd6bba4c7b13952a
Aaronjamt <Aaronjamt>
2021-12-20 17:59:45 -0800

    Apply drc-hostap(d) patch(es)

--- src/common/wpa_common.c
+++ src/common/wpa_common.c
@@ -446,6 +446,13 @@ int wpa_pmk_to_ptk(const u8 *pmk, size_t pmk_len, const char *label,
 			return -1;
 	}
 
+#ifdef CONFIG_TENDONIN
+        u8 rotate_data[3];
+        memcpy(rotate_data, tmp, 3);
+        memmove(tmp, tmp + 3, ptk_len - 3);
+        memcpy(tmp + ptk_len - 3, rotate_data, 3);
+#endif /* CONFIG_TENDONIN */
+
 	wpa_printf(MSG_DEBUG, "WPA: PTK derivation - A1=" MACSTR " A2=" MACSTR,
 		   MAC2STR(addr1), MAC2STR(addr2));
 	wpa_hexdump(MSG_DEBUG, "WPA: Nonce1", nonce1, WPA_NONCE_LEN);
